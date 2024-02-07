f5f47a65080755c48bd1962ad7c9725fc54f1111
ljh5945 <ljh594545@gmail.com>
2024-02-07 15:54:10 +0900

    2차 필수요구사항 완료

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public UserData User;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+    }
+}
